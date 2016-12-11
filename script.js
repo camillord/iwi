bb7a461b184180802a46fd924f4dd08d101e5484
camil <kamildabrowka@gmail.com>
2016-12-11 17:29:31 +0100

    Miesny jez

--- script.js
+++ script.js
@@ -2,18 +2,33 @@
  * Created by Kamil on 22.10.2016.
  */
 
-Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/3d-scatter.csv', function (err, rows) {
-    function unpack(rows, key) {
-        return rows.map(function (row) {
-            return row[key];
+Plotly.d3.json('/Data/nodes.json', function (err, rows) {
+
+    function getPersons(data, key) {
+        return data.map(function (row) {
+            if (row.type === 'person') {
+                return row[key];
+            }
         });
     }
 
+    function getExpertise(data, key) {
+        return data.map(function (row) {
+            if (row.type === 'expertise') {
+                return row[key];
+            }
+        });
+    }
+
+    function getNodes(data, key) {
+        return [data.source[key], data.target[key]];
+    }
+
     var data1 = {
-        x: unpack(rows, 'x1'), y: unpack(rows, 'y1'), z: unpack(rows, 'z1'),
+        x: getPersons(rows[0], 'x'), y: getPersons(rows[0], 'y'), z: getPersons(rows[0], 'z'),
         mode: 'markers',
         marker: {
-            size: 12,
+            size: 3,
             line: {
                 color: 'rgba(217, 217, 217, 0.14)',
                 width: 0.5
@@ -23,11 +38,11 @@ Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/3d-scatt
         type: 'scatter3d'
     };
     var data2 = {
-        x: unpack(rows, 'x2'), y: unpack(rows, 'y2'), z: unpack(rows, 'z2'),
+        x: getExpertise(rows[0], 'x'), y: getExpertise(rows[0], 'y'), z: getExpertise(rows[0], 'z'),
         mode: 'markers',
         marker: {
             color: 'rgb(127, 127, 127)',
-            size: 12,
+            size: 5,
             symbol: 'circle',
             line: {
                 color: 'rgb(204, 204, 204)',
@@ -37,7 +52,27 @@ Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/3d-scatt
         },
         type: 'scatter3d'
     };
+    var edges = [];
     var data = [data1, data2];
+
+    for (var i = 0; i < rows[1].length; i++) {
+
+        var edge = {
+            type: 'scatter3d',
+            mode: 'lines',
+            x: getNodes(rows[1][i], 'x'),
+            y: getNodes(rows[1][i], 'y'),
+            z: getNodes(rows[1][i], 'z'),
+            opacity: 1,
+            line: {
+                width: 1,
+                color: 'red',
+                reversescale: false
+            }
+        };
+        data.push(edge)
+    }
+
     var layout = {
         margin: {
             l: 0,
